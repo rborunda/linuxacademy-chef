e7263df00f53ed8836756651129520dfe323039c
rborunda <rborunda@gmail.com>
2016-07-20 05:27:16 +0000

    7.19.16 - A

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -3,7 +3,7 @@
 default["apache"]["sites"]["rborunda-gmail-com2"] = { "site_title" => "rborunda2 website coming soon", "port" => 80, "domain" => "rborunda-gmail-com2.mylabserver.com" }
 default["apache"]["sites"]["rborunda-gmail-com2b"] = {"site_title" => "rborunda2b website coming soon!", "port" => 80, "domain" => "rborunda-gmail-com2b.mylabserver.com"}
 
-default["apache"]["sites"]["rborunda-gmail-com4"] = {"site_title" => "rborunda4 website is coming soon", "port" => 80, "domain" => "rborunda-gmail-com4.mylabserver.com"} 
+default["apache"]["sites"]["rborunda-gmail-com3"] = {"site_title" => "rborunda3 website is coming soon", "port" => 80, "domain" => "rborunda-gmail-com3.mylabserver.com"} 
 
 default["author"]["name"] = "ramon"
 
